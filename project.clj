474b4c49d308161dfce70734e86d27fcc7603152
Dan Lidral-Porter <dan.lidral-porter@puppetlabs.com>
2014-09-12 10:33:07 -0700

    Revert liberator dep to 0.11.1
    This is the newest release used by our clojure projects, so use that
    instead of 0.12.x in order to avoid introducing dependency conflicts
    that have to be worked around downstream.

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
-                 [liberator "0.12.1"]]
+                 [liberator "0.11.1"]]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}}
   :deploy-repositories [["releases" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/releases/")]
                         ["snapshots" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/")]])
