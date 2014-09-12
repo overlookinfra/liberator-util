7d6dd64741b7899d3428d534f3328e14e2b69a89
Dan Lidral-Porter <dan.lidral-porter@puppetlabs.com>
2014-09-11 17:25:49 -0700

    Add deployment repositories to project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,10 @@
+(defn deploy-info
+  [url]
+  {:url url
+   :username :env/nexus_jenkins_username
+   :password :env/nexus_jenkins_password
+   :sign-releases false})
+
 (defproject liberator-util "0.1.0-SNAPSHOT"
   :description "A library to coordinate custom rendering in liberator."
   :url "https://github.com/aperiodic/liberator-util"
@@ -6,4 +13,6 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
                  [liberator "0.12.1"]]
-  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}}
+  :deploy-repositories [["releases" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/releases/")]
+                        ["snapshots" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/")]])
